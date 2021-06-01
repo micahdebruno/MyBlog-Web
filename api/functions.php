d55fb8e6482e2b1bc848fcb1982161c289dcad7b
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2021-06-01 08:10:44 -0700

    getUserDetail

--- api/functions.php
+++ api/functions.php
@@ -62,3 +62,14 @@ function isEmailExist($email){
         die("Query Failed". mysqli_error($result));
     }
 }
+
+function getUserDetailByEmail($email){
+    global $con;
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($con, $query);
+    if($result){
+        return $result;
+    }else{
+        return false;
+    }
+}
